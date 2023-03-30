e328796f6ef6b159651e1a066acbc831076975a4
tirgani12 <ahmed.abhr6@gmail.com>
2023-03-29 20:54:24 -0400

    second

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
         audi.color="balck";
         audi.model="A3";
         audi.power="5000cc";
+        Engine x=new Engine();
+        x.cylinder="0";
+        x.size="1234";
+        x.weight="6453k";
+        System.out.println(x);
+
     }
 }
